6acefe06a99aad266736a178fc1b5dc1cc2ceee6
vsantos1711 <vsantos067100@gmail.com>
2022-08-26 20:08:21 -0300

    Perguntas Frequentes

--- JS/script.js
+++ JS/script.js
@@ -21,3 +21,24 @@ function ativarProduto(parametro){
     }
 }
 parametros.forEach(ativarProduto);
+
+
+//Perguntas Frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle('ativo')
+    const ativo = resposta.classList.contains('ativo');
+    pergunta.setAttribute('aria-expanded', ativo);
+}
+
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
